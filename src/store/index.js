bf36d3d61dd259bb21f6382df49f077502982a8d
bens <5878794@qq.com>
2021-12-24 10:43:09 +0800

    add

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	// 全局属性变量
+	state: {
+		test:0
+	},
+	//同步方法
+	mutations: {
+		add(state){
+			state.test++;
+		}
+	},
+	//异步方法
+	actions: {
+
+	},
+	//获取计算的属性 vue3还在不在
+	getters:{
+
+	},
+	modules: {
+
+	}
 })
